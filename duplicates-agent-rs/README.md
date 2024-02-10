c266789a89f98174a95102be50974d1c112681c5
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2024-02-10 19:29:49 +0100

    Introduce calculate_duplicate_fast

--- duplicates-agent-rs/README.md
+++ duplicates-agent-rs/README.md
@@ -4,11 +4,11 @@ Implements the [duplicates detection agent](https://github.com/SebastianAigner/r
 
 - It's quick!
   - Kotlin: 7m 32s
-  - Rust: 52s (~8.7 times faster)
+  - Rust: 40s (~11.3 times faster) ~~52s (~8.7 times faster)~~
 
 # TODO
 - [ ] Use semaphores to not request all hashes at once (right now they're all done in a single burst)
 - [ ] Integrate with the persisted duplicates API (once it's done)
 - [ ] Cross-compile this for my low-end devices (Raspberry Pi 3 / 4)
 - [x] Apply [`Arc<str>` optimization](https://www.youtube.com/watch?v=A4cKi7PTJSs)
-- [ ] Apply early-terminating `mininum_cumulative_sum` to save a bunch of computations (at the expense of tracking state)
+- [x] Apply early-terminating `mininum_cumulative_sum` to save a bunch of computations (at the expense of tracking state)
--- duplicates-agent-rs/src/main.rs
+++ duplicates-agent-rs/src/main.rs
@@ -55,7 +55,7 @@ async fn main() {
         network_hashes
     };
 
-    let hashes: HashMap<Arc<str>, Vec<DHash>> = hashes.clone().iter().map(|(k,v)| {
+    let hashes: HashMap<Arc<str>, Vec<DHash>> = hashes.clone().iter().map(|(k, v)| {
         let key: Arc<str> = k.clone().into_boxed_str().into();
         (key, v.clone())
     }).collect();
@@ -103,11 +103,12 @@ async fn main() {
     }
     final_vec.sort_by(|a, b| a.distance.cmp(&b.distance));
     // let candidates: Vec<_> = final_vec.iter().take(10).collect();
-    let outstr = final_vec[..10]
+    let out_string = final_vec
         .iter()
+        .filter(|x| { x.distance == 0 })
         .map(|x| format!("{} -{}-> {}", x.a, x.distance, x.b))
         .join("\n");
-    println!("{outstr}");
+    println!("{out_string}");
     let done = start.elapsed();
     println!("Done in {done:?}");
 }
@@ -123,7 +124,7 @@ fn spawn_worker(shared: Arc<RunnerInner>) -> JoinHandle<()> {
     tokio::spawn(async move {
         loop {
             if let Ok((id, curr_hashes)) = shared.recv_chan.recv().await {
-                let res = calculate_duplicate(&id, &curr_hashes, &shared.hashes);
+                let res = calculate_duplicate_fast(&id, &curr_hashes, &shared.hashes);
                 println!("{id} {res:?}");
 
                 if let Some(other) = res {
@@ -162,13 +163,17 @@ fn calculate_duplicate(
 
     let handful: Vec<_> = current_hashes.choose_multiple(&mut rng, 100).collect();
     let all_other_hashes = all_hashes.iter().filter(|(id, _)| id != &current_id);
-    let minimal_deviations_from_this_hash = all_other_hashes
-        .filter(|(_, hashes)| !hashes.is_empty())
+    let all_nonempty_ids = all_other_hashes
+        .filter(|(_, hashes)| !hashes.is_empty());
+    let minimal_deviations_from_this_hash = all_nonempty_ids
         .map(|(other_hash_id, other_hash)| {
-            let devation_from_this_hash: u32 = handful
-                .iter()
-                .map(|&&sample| minimal_distance(other_hash, sample))
-                .sum();
+            let devation_from_this_hash: u32 =
+                handful
+                    .iter()
+                    .map(|&&sample| {
+                        minimal_distance(other_hash, sample)
+                    })
+                    .sum();
             (other_hash_id, devation_from_this_hash)
         });
 
@@ -181,6 +186,51 @@ fn calculate_duplicate(
         id: id_with_smallest_deviation.clone(),
         distance: deviation,
     })
+}
+
+/// Fast implementation of calculate_duplicate.
+/// Where calculate_duplicate evaluates all distances before determining the minimum,
+/// calculate_duplicate_fast stops its computations for a given hash when it's clear
+/// that the intermediate computation result already disqualifies it as the shortest distance,
+/// and moves on directly to the next group to be tested.
+fn calculate_duplicate_fast(
+    current_id: &Arc<str>,
+    current_hashes: &Vec<DHash>,
+    all_hashes: &HashMap<Arc<str>, Vec<DHash>>,
+) -> Option<IdWithDistance> {
+    let mut rng: ThreadRng = thread_rng();
+    if current_hashes.is_empty() {
+        return None;
+    }
+
+    let handful_hashgroup: Vec<_> = current_hashes.choose_multiple(&mut rng, 100).collect();
+    let all_other_hashes = all_hashes.iter().filter(|(id, group)| id != &current_id && !group.is_empty());
+    let mut current_minimum_distance = u32::MAX;
+    let mut current_minimum_entry_id: Arc<str> = "none".into();
+
+    // we take a handful of hashes
+    // for those 100 hashes, look at other-hashgroup.
+    // look at each handful-entry. find the CLOSEST match in the other-hashgroup. sum up those closest match distances.
+    // the accumulation of those is the total minimal distance between the handful and other-hashgroup.
+    for (other_entry_id, other_hashgroup) in all_other_hashes {
+        let mut distance_accumulator = 0;
+        for this_dhash in &handful_hashgroup {
+            if distance_accumulator >= current_minimum_distance {
+                break;
+            }
+            let minimal_distance = minimal_distance_to_group(**this_dhash, other_hashgroup);
+            distance_accumulator += minimal_distance;
+        }
+        if distance_accumulator < current_minimum_distance {
+            // println!("found closer distance, going from {} to {}", current_minimum_distance, cum_dist);
+            current_minimum_distance = distance_accumulator;
+            current_minimum_entry_id = other_entry_id.clone();
+        }
+    }
+    return Some(IdWithDistance {
+        id: current_minimum_entry_id,
+        distance: current_minimum_distance,
+    });
 }
 
 async fn get_all_hashes() -> HashMap<String, Vec<DHash>> {
@@ -218,6 +268,10 @@ impl DHash {
         let xorred = self.raw ^ other.raw;
         xorred.count_ones()
     }
+}
+
+fn minimal_distance_to_group(from: DHash, to_hashes: &[DHash]) -> u32 {
+    minimal_distance(to_hashes, from)
 }
 
 fn minimal_distance(hashes: &[DHash], target: DHash) -> u32 {
