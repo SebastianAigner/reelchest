9c137c936976dbf78bb5cd3ceca2adb29f067007
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2024-03-07 01:07:12 +0100

    Use NIO's FileOutputChannel to directly move bytes from the ByteReadChannel to the File (avoiding intermediate ByteArray operations) Move file write to IO dispatcher (NIO channels are synchronous by default!)

--- backend/src/main/kotlin/downloader/Downloader.kt
+++ backend/src/main/kotlin/downloader/Downloader.kt
@@ -3,13 +3,15 @@ package io.sebi.downloader
 import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.http.*
-import io.ktor.util.*
 import io.ktor.utils.io.*
 import io.sebi.network.NetworkManager
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.withContext
 import kotlinx.coroutines.yield
 import org.slf4j.LoggerFactory
 import java.io.File
+import java.io.FileOutputStream
 
 
 class Downloader(val networkManager: NetworkManager) {
@@ -19,17 +21,20 @@ class Downloader(val networkManager: NetworkManager) {
         file: File = File("temp.jpg"),
         absoluteProgressCallback: ((Pair<Long, Long?>) -> Unit),
     ) = coroutineScope {
+        val fileOutputChannel = FileOutputStream(file).channel
         networkManager.getRawClient(noReallyItsOkay = true).prepareGet(url).execute {
             val chan = it.body<ByteReadChannel>()
             var ctr = 0L
 
-            while (!chan.isClosedForRead) {
-                yield()
-                chan.read { buf ->
-                    val ba = buf.moveToByteArray()
-                    file.appendBytes(ba)
-                    ctr += ba.size
-                    absoluteProgressCallback(ctr to it.contentLength())
+            withContext(Dispatchers.IO) {
+                while (!chan.isClosedForRead) {
+                    yield()
+                    chan.read { buf ->
+                        val rem = buf.remaining()
+                        fileOutputChannel.write(buf) // This is a synchronous operation
+                        ctr += rem
+                        absoluteProgressCallback(ctr to it.contentLength())
+                    }
                 }
             }
 
