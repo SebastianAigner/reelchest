ba69fac62d76a8cdf8f63f0992df58ebd1a7db14
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2024-12-26 19:20:23 +0100

    play with scene lifecycle in swiftui

--- mobileapp/iosApp/iosApp/iOSApp.swift
+++ mobileapp/iosApp/iosApp/iOSApp.swift
@@ -7,13 +7,26 @@ func foo() {
 
 @main
 struct iOSApp: App {
-	var body: some Scene {
-		WindowGroup {
-		    ZStack {
-		        Color.black.ignoresSafeArea(.all) // status bar color
-			    ContentView()
-                
-			}.preferredColorScheme(.light)
-		}
-	}
+    @Environment(\.scenePhase) var scenePhase
+    @State private var shouldRender: Bool = true // Boolean state
+
+
+    var body: some Scene {
+        WindowGroup {
+            ZStack {
+                Color.black.ignoresSafeArea(.all) // status bar color
+
+                ContentView()
+                    .opacity(shouldRender ? 1.0 : 0.0)
+            }
+            .preferredColorScheme(.light)
+            .onChange(of: scenePhase) { newPhase in
+                if newPhase != .active {
+                    shouldRender = false
+                } else {
+                    shouldRender = true
+                }
+            }
+        }
+    }
 }
