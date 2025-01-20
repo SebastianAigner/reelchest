6575323e440f30d1667bacf67910a85b71b9af78
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2025-01-20 14:14:15 +0100

    Refactor CORS import to use specific routing package

--- backend/src/main/kotlin/KtorPlugins.kt
+++ backend/src/main/kotlin/KtorPlugins.kt
@@ -5,7 +5,7 @@ import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.callloging.*
 import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.plugins.cors.*
+import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.plugins.partialcontent.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.request.*
