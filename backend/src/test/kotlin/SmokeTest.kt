c9bc3ebe47475f10dd6a698fa771e5edab0a7d94
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2025-01-25 16:48:10 +0100

    Add a smoke test that just checks whether the project even compiles (tooling, yay!)

---
+++ backend/src/test/kotlin/SmokeTest.kt
@@ -0,0 +1,13 @@
+package io.sebi.process
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MathTest {
+
+    @Test
+    fun `test addition of 2 and 2`() {
+        val result = 2 + 2
+        assertEquals(4, result, "2 + 2 should equal 4")
+    }
+}
