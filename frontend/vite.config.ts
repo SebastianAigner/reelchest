0c4f1fd023052b5fcf4b3ec364633287abaf9262
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2025-01-25 14:25:03 +0100

    Add proxy route for /ul in Vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     proxy: {
-      "/api": "http://localhost:8080"
+      "/api": "http://localhost:8080",
+      "/ul": "http://localhost:8080"
     }
   }
 })
