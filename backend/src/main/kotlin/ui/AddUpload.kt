5a0095a418ecf04f3b9122d440033079c25cba4d
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2025-03-16 20:43:54 +0100

    Use `unsafe` block for JavaScript htmx progress listener

--- backend/src/main/kotlin/ui/AddUpload.kt
+++ backend/src/main/kotlin/ui/AddUpload.kt
@@ -42,12 +42,14 @@ fun Route.addUpload() {
                         max = "100"
                     }
                     script {
-                        //language=JavaScript
-                        +"""
+                        unsafe {
+                            //language=JavaScript
+                            +"""
                                     htmx.on('#form', 'htmx:xhr:progress', function(evt) {
                                       htmx.find('#progress').setAttribute('value', evt.detail.loaded/evt.detail.total * 100)
                                     });
                         """.trimIndent()
+                        }
                     }
                 }
             }
